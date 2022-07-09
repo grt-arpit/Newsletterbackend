4128b295e11a3f3b132655869dd63426745b730f
Arpit Yadav <yadavarpit927@gmail.com>
2022-07-09 18:41:19 +0530

    9july

--- Routers/SuscribeRouter.js
+++ Routers/SuscribeRouter.js
@@ -5,7 +5,43 @@ const Model = require("../models/SubscribeModal");
 router.post("/sscrib",(req,res)=>{
     const data = req.body;
     console.log(req.body);
-    res.send("request processed in suscribe router");
+    // res.send("request processed in suscribe router");
+
+    //create opertion
+    new Model(data)
+    .save()
+    .then((result) => {
+        console.log("data saved");
+        res.json(result);
+    }).catch((err) => {
+        console.error("error");
+        res.json(err);
+    });
 });
+
+//this is used for feched all user data
+router.get("/getall", (req, res) => {
+    Model.find({})
+      .then((result) => {
+        console.log("user data fetched");
+        res.json(result);
+      })
+      .catch((err) => {
+        console.error("error");
+        res.json(err);
+      });
+  });
+router.get("/getbyownerid/:ownerid", (req, res) => {
+    Model.find({owner:req.params.ownerid})
+      .then((result) => {
+        console.log("user data fetched");
+        res.json(result);
+      })
+      .catch((err) => {
+        console.error("error");
+        res.json(err);
+      });
+  });
+
 
 module.exports=router;
--- models/SubscribeModal.js
+++ models/SubscribeModal.js
@@ -1,10 +1,10 @@
 
-const {Schema,model } = require('../connection');  //importing moongoose but instead of mongoose i imported its predifine keys
+const {Schema,model, Types } = require('../connection');  //importing moongoose but instead of mongoose i imported its predifine keys
 
 const schema = new Schema({
     email:String,
     name:String,
-    owner:String,
+    owner:{type : Types.ObjectId, ref:"users"},
 });
 
 module.exports= model("newsletter", schema);
